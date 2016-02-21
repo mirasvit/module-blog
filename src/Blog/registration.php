d75abe6544ee03c88e1e0ee516acc9e907ba8960
Alexander Drok <alx@mirasvit.com>
2016-02-22 01:03:12 +0200

    Initial commit

---
+++ src/Blog/registration.php
@@ -0,0 +1,7 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+        \Magento\Framework\Component\ComponentRegistrar::MODULE,
+        'Mirasvit_Blog',
+        __DIR__
+    );
