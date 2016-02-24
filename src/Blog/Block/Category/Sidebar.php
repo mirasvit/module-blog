e8c167bff525e01dfee971be4058dcfb3a131a3d
Alexander Drok <alx@mirasvit.com>
2016-02-25 01:59:13 +0200

    Category Sidebar

---
+++ src/Blog/Block/Category/Sidebar.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Mirasvit\Blog\Block\Category;
+
+use Magento\Framework\View\Element\Template;
+use Magento\Framework\Registry;
+use Magento\Framework\View\Element\Template\Context;
+use Mirasvit\Blog\Model\ResourceModel\Category\CollectionFactory as CategoryCollectionFactory;
+
+class Sidebar extends Template
+{
+    /**
+     * @var CategoryCollectionFactory
+     */
+    protected $categoryCollectionFactory;
+
+    /**
+     * @var Registry
+     */
+    protected $registry;
+
+    /**
+     * @var Context
+     */
+    protected $context;
+
+    /**
+     * @param CategoryCollectionFactory $categoryCollectionFactory
+     * @param Registry                  $registry
+     * @param Context                   $context
+     * @param array                     $data
+     */
+    public function __construct(
+        CategoryCollectionFactory $categoryCollectionFactory,
+        Registry $registry,
+        Context $context,
+        array $data = []
+    ) {
+        $this->categoryCollectionFactory = $categoryCollectionFactory;
+        $this->registry = $registry;
+        $this->context = $context;
+
+        parent::__construct($context, $data);
+    }
+
+    /**
+     * @return \Mirasvit\Blog\Model\Category[]
+     */
+    public function getTree()
+    {
+        return $this->categoryCollectionFactory->create()
+            ->getTree();
+    }
+
+    /**
+     * @return \Mirasvit\Blog\Model\Category|false
+     */
+    public function getCurrentCategory()
+    {
+        return $this->registry->registry('current_blog_category');
+    }
+
+    /**
+     * @param \Mirasvit\Blog\Model\Category $category
+     * @return bool
+     */
+    public function isCurrent($category)
+    {
+        if ($this->getCurrentCategory() && $this->getCurrentCategory()->getId() == $category->getId()) {
+            return true;
+        }
+
+        return false;
+    }
+}
