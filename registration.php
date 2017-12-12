86253602a28d7dd415292ebf85d9e120013fbab5
vtruhmanov <vtruhmanov@mirasvit.com>
2017-12-12 19:44:24 +0200

    Composer Installation Check fix

--- registration.php
+++ registration.php
@@ -1,4 +1,9 @@
 <?php
+$registration = dirname(__DIR__, 4) . '/vendor/mirasvit/module-blog/registration.php';
+if (file_exists($registration)) {
+    # module was already installed via composer
+    return;
+}
 
 \Magento\Framework\Component\ComponentRegistrar::register(
         \Magento\Framework\Component\ComponentRegistrar::MODULE,
