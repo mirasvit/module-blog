39a214830cb8a5cc80074b06a839901a88e336d5
Alexander Drok <alx@mirasvit.com>
2018-01-02 12:00:39 +0200

    fix: XSS vulnerability in search form

--- registration.php
+++ registration.php
@@ -1,10 +1,4 @@
 <?php
-$registration = dirname(dirname(dirname(__DIR__))) . '/vendor/mirasvit/module-blog/registration.php';
-if (file_exists($registration)) {
-    # module was already installed via composer
-    return;
-}
-
 \Magento\Framework\Component\ComponentRegistrar::register(
         \Magento\Framework\Component\ComponentRegistrar::MODULE,
         'Mirasvit_Blog',
